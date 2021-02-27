bd878a439c52472cd8c16802c086e17ca010b1f8
liqi <191728146@qq.com>
2021-02-27 09:06:52 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { HashRouter, Switch, Route, Link } from 'react-router-dom'
+import Website from './pages/Website'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HashRouter>
+        <Switch>
+          <Route path='/' exact>
+            <div>
+
+              <h1>李奇的网站</h1>
+
+              <ul>
+                <li>
+                  <Link to='/'>首页</Link>
+                </li>
+                <li>
+                  <Link to='/website'>常用网站</Link>
+                </li>
+              </ul>
+
+            </div>
+          </Route>
+          <Route path='/website'>
+            <Website />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/Website/index.jsx
@@ -0,0 +1,18 @@
+
+function Website() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="https://wangdoc.com/" target="_blank" rel='noreferrer'>网道</a>
+                </li>
+                <li>
+                    <a href="https://es6.ruanyifeng.com/" target="_blank" rel='noreferrer'>ES6入门教程</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Website;
